bb3a60b312af8bd0101896131b1d5c402a4c77cc
nubela <nubela@gmail.com>
2015-12-22 02:54:19 +0800

    updated run quilt file to always update project

--- scripts/run_quilt.sh
+++ scripts/run_quilt.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+cd /workspace/docker-quilt && git pull
+cd /workspace/docker-quilt && pip install -r requirements.txt
+PYTHONPATH=/workspace/docker-quilt python /workspace/docker-quilt/quilt/cli/manage.py reset True
 PYTHONPATH=/workspace/docker-quilt python /workspace/docker-quilt/quilt/cli/manage.py deploy
